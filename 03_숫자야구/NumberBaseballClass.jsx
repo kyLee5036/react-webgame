5ceabce4da8833bb72875a9eab26ec61be0da2b6
LEEKY <37015435+KeonYoungLeee@users.noreply.github.com>
2019-10-24 11:31:57 +0900

    Add BumberBaseballClass.jsx when Map part

--- 03_숫자야구/NumberBaseballClass.jsx
+++ 03_숫자야구/NumberBaseballClass.jsx
@@ -1,13 +1,55 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+function getNumbers () {
+    
+}
+
 class NumberBaseballClass extends Component {
-    state = {
-        text:'Hello webpa!!!23233',
-    }
+   state = {
+       result: '',
+       value: '',
+       answer: getNumbers(),
+       tries : [],
+   }
+
+   onSumbitForm = () => {
+
+   };
+
+   onChangeInput = () => {
+
+   };
 
     render() {
-        return <h3>{this.state.text}</h3>
+        return (
+        <>
+            <h1>{this.state.result}</h1>
+            <form onSubmit={this.onSumbitForm}>
+                <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+            </form>
+            <div>시도 : {this.state.tries.length}</div>
+            <ul>{[
+                    { fruit:'사과', taste:'맛있다' },
+                    { fruit:'바나나', taste:'맛있다' },
+                    { fruit:'포도', taste:'시다' },
+                    { fruit:'귤', taste:'시다' },
+                    { fruit:'감', taste:'떫다' },
+                    { fruit:'배', taste:'달다' },
+                    ].map((value, index) => {     
+                        return ( 
+                            <li key={value.fruit + value.taste}><b>{value.fruit}</b> - {value.taste}</li>
+                        );
+                })}
+                {/* <li><b>사과</b> - 맛있다</li>
+                <li><b>바나나</b> - 맛있다</li>
+                <li><b>포도</b> - 시다</li>
+                <li><b>귤</b> - 시다</li>
+                <li><b>감</b> - 떫다</li>
+                <li><b>배</b> - 달다</li> */}
+            </ul>
+        </>
+        )
     }
 }
 
