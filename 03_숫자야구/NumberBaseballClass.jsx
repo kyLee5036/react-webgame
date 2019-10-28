51e4d6c17b087be6d39d421a3e8229788bd15391
LEEKY <37015435+KeonYoungLeee@users.noreply.github.com>
2019-10-28 10:40:06 +0900

    modify refactoring for data structure

--- 03_숫자야구/NumberBaseballClass.jsx
+++ 03_숫자야구/NumberBaseballClass.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Try from './Try'
 
-function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+const getNumbers = () => { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
     const candidate = [1,2,3,4,5,6,7,8,9];
     const array = [];
     for (let i = 0; i < 4; i += 1) {
@@ -24,11 +24,12 @@ class NumberBaseballClass extends Component {
    }
      
     onSubmitForm = (event) => {
+        const {result, value, tries, answer} = this.state;
         event.preventDefault();
-        if ( this.state.value === this.state.answer.join('')) {
+        if ( value === answer.join('')) {
             this.setState({
                 result:'HOMERUN!!!',
-                tries: [...this.state.tries, {try: this.state.value, result:'HOMERUN!!!'}],
+                tries: [...tries, {try: value, result:'HOMERUN!!!'}],
             });
             alert('HOMERUN');
             this.setState({
@@ -37,12 +38,12 @@ class NumberBaseballClass extends Component {
                 tries: [],
             });
         } else {
-            const answerArray = this.state.value.split('').map( (v) => parseInt(v) );
+            const answerArray = value.split('').map( (v) => parseInt(v) );
             let strike = 0;
             let ball = 0;
-            if (this.state.tries.length >= 9 ) {
+            if (tries.length >= 9 ) {
                 this.setState({
-                    result: `More than 10 : ${this.state.answer.join(',')}`,
+                    result: `More than 10 : ${answer.join(',')}`,
                 });
                 alert('Game restart');
                 this.setState({
@@ -52,14 +53,14 @@ class NumberBaseballClass extends Component {
                 });
             } else {
                 for ( let i = 0; i< 4; i+=1) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball+=1;
                     }
                 }
                 this.setState({
-                    tries: [...this.state.tries, {try: this.state.value, result:`strike : ${strike}, ball : ${ball}`}],
+                    tries: [...tries, {try: value, result:`strike : ${strike}, ball : ${ball}`}],
                     value : '',
                 });
             }
@@ -75,14 +76,15 @@ class NumberBaseballClass extends Component {
 
 
     render() {
+        const {result, value, tries} = this.state;
         return (
         <>
-            <h1>{this.state.result}</h1>
+            <h1>{result}</h1>
             <form onSubmit={this.onSubmitForm}>
-                <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                <input maxLength={4} value={value} onChange={this.onChangeInput}/>
             </form>
-            <div>시도 : {this.state.tries.length}</div>
-            <ul>{this.state.tries.map((value, index) => {  // 변경
+            <div>시도 : {tries.length}</div>
+            <ul>{tries.map((value, index) => {  // 변경
                     return ( 
                         <Try key={`${index + 1} trying`} tryInfo={value} />
                     );
