aa71488a171f900ad95f8aa5624e78a1119b3799
LEEKY <37015435+KeonYoungLeee@users.noreply.github.com>
2019-11-19 13:12:37 +0900

    Add React조건문사용방법 및 에러수정

--- 04_반응속도체크/README.md
+++ 04_반응속도체크/README.md
@@ -1,10 +1,43 @@
 # 반응속도체크
 
-+ React 조건문
-+ setTimeout 넣어 반응속도 체크
+
++ [React 조건문](#React-조건문)
++ [setTimeout 넣어 반응속도 체크](#setTimeout-넣어-반응속도-체크)
 + 성능 체크와 Q&amp;A
 + 반응속도체크 Hooks 전환
 
 ## React 조건문
 
-render(JSX에서는)안 에서는 for문, if문을 사용할 수 없다. 
+render(JSX에서는)안 에서는 for문, if문을 사용할 수 없다. 쓸 수 있는 방법은 있는데, 지저분해진다. <br>
+
+### 조건문 사용방법
+<strong>빈 배열</strong> 경우에는 합계, 평균 구할 수 없다. 그래서 div태그를 안 보여주게한다.
+```javascript
+<div>average time : {this.state.result.reduce((a,c) => a + c / this.state.result.length )}ms</div>
+```
+false, undefined, null은 jsx에서 태그없음을 의미한다.
+리액트에서의 조건문 사용하는 방법 (삼항 연산자를 사용한다.)
+```javascript
+{this.state.result.length === 0 
+    ? null
+    : <div>average time : {this.state.result.reduce((a,c) => a + c / this.state.result.length )}ms</div>}  
+```
+
+함수를 사용하는 경우도 있다.
+
+```javascript
+renderAverge = () => {
+    this.state.result.length === 0 
+    ? null
+    : <div>average time : {this.state.result.reduce((a,c) => a + c / this.state.result.length )}ms</div>
+  }
+
+ render() {
+    return (
+      <>
+        {this.renderAverge()}  
+      </>
+    )
+  }
+```
+### setTimeout 넣어 반응속도 체크
--- 04_반응속도체크/ResponseCheckClass.jsx
+++ 04_반응속도체크/ResponseCheckClass.jsx
@@ -1,24 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class ResponseCheckClass extends Component {
-    state = {
+  state = {
+    state: 'waiting',
+    message: '클릭해서 시작하세요.',
+    result: [],
+  };
+
+  
+
+  onClickScreen = () => {
+    const {state, message, result} = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state : 'ready',
+        message : '초록색이 되면 출력하세요',
+      });
+      setTimeout( ()=> {
+       this.setState({
+        state : 'now',
+        message : '지금 출력',
+       }); 
+      }, Math.floor(Math.random() * 1000) + 2000);
+    }else if ( state==='ready' ) {
+      this.setState({
         state:'waiting',
-        message: '클릭해서 시작하세요.',
+        message : '너무 성급하시군요! 초록색이 된 후에 출력하세요.',
         result: [],
-    };
+      });
+    }else if ( state==='now' ) {
+      this.setState({
+        state:'waiting',
+        message : '클릭해서 시작하세요.',
+        result: [],
+      });
+    }
+  }
 
-    render() {
 
-        return (
-          <>
-           <div 
-              id="screen"
-              className="">
+  renderAverge = () => {
+    this.state.result.length === 0 
+    ? null
+    : <div>average time : {this.state.result.reduce((a,c) => a + c / this.state.result.length )}ms</div>
+  }
+  
 
-           </div>
-          </>
-        )
-      }
+  render() {
+
+    return (
+      <>
+        <div
+          id ="screen"
+          className={this.state.state}
+          onClick={this.onClickScreen}
+        >
+          {this.state.message}
+        </div>
+        {this.renderAverge()}  
+        
+      </>
+    )
+  }
 }
 
 
