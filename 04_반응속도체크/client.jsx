1ae1b1c26cdfec0f721a19cf1d7f647fc00d0192
LEEKY <37015435+KeonYoungLeee@users.noreply.github.com>
2019-11-21 13:30:35 +0900

    Add 반응속도체크 (class -> hooks변환, useState랑 useRef의 차이)

--- 04_반응속도체크/client.jsx
+++ 04_반응속도체크/client.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import {hot} from 'react-hot-loader/root';
 
 import ResponseCheckClass from './ResponseCheckClass';
+import ResponseCheckHooks from './ResponseCheckHooks';
 
-const HotResponseCheckClass = hot(ResponseCheckClass);
+
+const HotResponseCheckClass = hot(ResponseCheckHooks);
 
 ReactDOM.render(<HotResponseCheckClass/> ,document.querySelector('#root'));
